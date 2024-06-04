6c146ff4583e4bfce969c9c281daa5ce3c6b5fbd
DESKTOP-R9A6VT6\HP <tonghuor.heng97@gmail.com>
2024-06-04 21:27:04 +0700

    Initialized project vite + vue + vuetify

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <DataTable/>
+</template>
+
+<script>
+import DataTable from './components/DataTable.vue'
+export default {
+  name: 'App',
+
+  components: {
+    DataTable,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
